fbe25972aa4caa8027c225b0b7323bbd18e14bf8
damianomal <damiano.malafronte@gmail.com>
2017-02-28 15:18:50 +0100

    Updated something
    It is surely meaningful.

--- shearlets_samples/shearlet_sample_clustering_full.m
+++ shearlets_samples/shearlet_sample_clustering_full.m
@@ -1,10 +1,4 @@
 
-% VID = load_video_to_mat('person02_jogging_d3_uncomp.avi');
-% shearlet_video_clustering_full( VID(:,:,1:100), SORT_CTRS, 'jogging', true);
-
-% VID = load_video_to_mat('person09_boxing_d4_uncomp.avi');
-% shearlet_video_clustering_full( VID(:,:,1:100), SORT_CTRS, 'boxing_quick', true);
-
 filenames = {'person02_boxing_d1_uncomp', 'person02_boxing_d2_uncomp', 'person01_handclapping_d4_uncomp', ...
              'person05_handwaving_d3_uncomp', 'person04_jogging_d4_uncomp', 'person04_running_d1_uncomp', ...
              'person02_walking_d3_uncomp', 'person01_walking_d2_uncomp'};
